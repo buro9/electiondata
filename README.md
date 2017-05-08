5312364d093a42afe1504d94b16e1100f5cb7389
David Kitchen <dkitchen@cloudflare.com>
2017-05-08 22:23:36 +0100

    Added parsing of 2015 data
    Renamed 2010 data for consistency

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 For 2010 and 2015, formatted in a consistent way.
 
 ```bash
-go run main.go -2010=raw/2010/CONSTITUENCY.csv
+go run main.go -2010=raw/2010/RESULTS.csv
+go run main.go -2010=raw/2015/RESULTS.csv
 ```
--- main.go
+++ main.go
@@ -12,17 +12,21 @@ import (
 )
 
 const (
-	CON  = "CON"
-	DUP  = "DUP"
-	GRN  = "GRN"
-	IND  = "IND"
-	LAB  = "LAB"
-	LIB  = "LIB"
-	PC   = "PC"
-	SDLP = "SDLP"
-	SF   = "SF"
-	SNP  = "SNP"
-	SPK  = "SPK"
+	ALL   = "ALL"
+	CON   = "CON"
+	DUP   = "DUP"
+	GRN   = "GRN"
+	IND   = "IND"
+	LAB   = "LAB"
+	LABCO = "LABCO"
+	LIB   = "LIB"
+	PC    = "PC"
+	SDLP  = "SDLP"
+	SF    = "SF"
+	SNP   = "SNP"
+	SPK   = "SPK"
+	UKIP  = "UKIP"
+	UUP   = "UUP"
 )
 
 type Results struct {
@@ -130,6 +134,8 @@ func (results *Results) parse2010() error {
 		res.Elected = (record[ELECTED] == "*")
 		res.Incumbent = (record[INCUMBENT] == "*")
 		switch record[PARTY] {
+		case "Alliance":
+			res.Party = ALL
 		case "C":
 			res.Party = CON
 		case "DUP":
@@ -140,6 +146,8 @@ func (results *Results) parse2010() error {
 			res.Party = IND
 		case "Lab":
 			res.Party = LAB
+		case "Lab Co-op":
+			res.Party = LABCO
 		case "LD":
 			res.Party = LIB
 		case "PC":
@@ -169,13 +177,13 @@ func (results *Results) parse2010() error {
 			if r.Elected {
 				// winning parties sanitisation test
 				switch r.Party {
-				case "Alliance":
+				case ALL:
 				case CON:
 				case DUP:
 				case GRN:
 				case IND:
 				case LAB:
-				case "Lab Co-op":
+				case LABCO:
 				case LIB:
 				case PC:
 				case SDLP:
@@ -193,6 +201,126 @@ func (results *Results) parse2010() error {
 }
 
 func (results *Results) parse2015() error {
+	b, err := readFile(*raw2015)
+	if err != nil {
+		return err
+	}
+
+	r := csv.NewReader(bytes.NewReader(b))
+	records, err := r.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	const (
+		FORENAME = iota
+		SURNAME
+		DESCRIPTION
+		SEAT
+		PANO
+		VOTE
+		PERCENT
+		CHANGE
+		FOO1
+		INCUMBENT
+		FOO2
+		SEAT_ID
+		REGION_ID
+		COUNTY
+		REGION
+		COUNTRY
+		SEAT_TYPE
+		PARTY_NAME
+		PARTY
+	)
+
+	var cur Constituency
+	for ii, record := range records {
+		if ii == 0 {
+			// header row
+			continue
+		}
+
+		var elected bool
+		if ii == 1 {
+			cur.Name = record[SEAT]
+			elected = true
+		} else if cur.Name != record[SEAT] {
+			// add the last one, and work on the new
+			results.Constituencies = append(results.Constituencies, cur)
+			cur = Constituency{}
+			cur.Name = record[SEAT]
+			elected = true
+		}
+
+		var res Result
+		res.Candidate = fmt.Sprintf("%s, %s", record[SURNAME], record[FORENAME])
+		res.Elected = elected
+		res.Incumbent = (record[INCUMBENT] == "MP")
+		switch record[PARTY] {
+		case "Con":
+			res.Party = CON
+		case "DUP":
+			res.Party = DUP
+		case "Green":
+			res.Party = GRN
+		case "Ind":
+			res.Party = IND
+		case "Lab":
+			res.Party = LAB
+		case "LD":
+			res.Party = LIB
+		case "PC":
+			res.Party = PC
+		case "SDLP":
+			res.Party = SDLP
+		case "SF":
+			res.Party = SF
+		case "SNP":
+			res.Party = SNP
+		case "Spk":
+			res.Party = SPK
+		case "UKIP":
+			res.Party = UKIP
+		case "UUP":
+			res.Party = UUP
+		default:
+			res.Party = record[PARTY]
+		}
+		votes, err := strconv.Atoi(strings.Replace(record[VOTE], ",", "", -1))
+		if err != nil {
+			return err
+		}
+		res.Votes = votes
+		cur.Results = append(cur.Results, res)
+	}
+	results.Constituencies = append(results.Constituencies, cur)
+
+	for _, c := range results.Constituencies {
+		for _, r := range c.Results {
+			if r.Elected {
+				// winning parties sanitisation test
+				switch r.Party {
+				case CON:
+				case DUP:
+				case GRN:
+				case IND:
+				case LAB:
+				case LIB:
+				case PC:
+				case SDLP:
+				case SF:
+				case SNP:
+				case SPK:
+				case UKIP:
+				case UUP:
+				default:
+					return fmt.Errorf("%s not recognised for %s\n", r.Party, c.Name)
+				}
+			}
+		}
+	}
+
 	return nil
 }
 
